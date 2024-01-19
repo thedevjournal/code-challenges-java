687e66b7f2ca7a74de69aa3041c4787a4acffb9f
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-01-19 21:49:00 +0530

    GH-54: Adds Tests for CodingBat - Warmup-1 - countXX

---
+++ codingbat/src/test/java/io/thedevjournal/java/codingbat/warmup2/CountXXTest.java
@@ -0,0 +1,89 @@
+package io.thedevjournal.java.codingbat.warmup2;
+
+import static io.thedevjournal.java.common.StringConstants.EMPTY_STRING;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CountXXTest {
+
+    private CountXX fixture;
+
+    @BeforeEach
+    void setUp() {
+        fixture = new CountXX();
+    }
+
+    @Test
+    void shouldReturnZero_WhenStringIsNull() {
+
+        final String str = null;
+
+        final int expected = 0;
+
+        final int actual = fixture.countXxValues(str);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnZero_WhenStringIsEmpty() {
+
+        final String str = EMPTY_STRING;
+
+        final int expected = 0;
+
+        final int actual = fixture.countXxValues(str);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnZero_WhenStringIsNotEmptyAndNoOccurrence() {
+
+        final String str = "abcd";
+
+        final int expected = 0;
+
+        final int actual = fixture.countXxValues(str);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnCountOne_WhenStringIsNotEmptyAndSingleOccurrence() {
+
+        final String str = "axxd";
+
+        final int expected = 1;
+
+        final int actual = fixture.countXxValues(str);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnCountOne_WhenStringIsNotEmptyAndMultipleOccurrenceNonOverlapping() {
+
+        final String str = "abxxcdxx";
+
+        final int expected = 2;
+
+        final int actual = fixture.countXxValues(str);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnCountOne_WhenStringIsNotEmptyAndMultipleOccurrenceOverlapping() {
+
+        final String str = "xxxx";
+
+        final int expected = 3;
+
+        final int actual = fixture.countXxValues(str);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
