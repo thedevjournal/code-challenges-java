a7e3bfdfaed65b3ddf9ec2017814455cb0a573d2
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-01-30 22:01:45 +0530

    GH-69: Fixes Codacy violation CodingBat-String1-deFront

--- codingbat/src/main/java/io/thedevjournal/java/codingbat/string1/DeFront.java
+++ codingbat/src/main/java/io/thedevjournal/java/codingbat/string1/DeFront.java
@@ -1,5 +1,6 @@
 package io.thedevjournal.java.codingbat.string1;
 
+import static io.thedevjournal.java.common.NumberConstants.ZERO;
 import static io.thedevjournal.java.common.NumberConstants.ONE;
 import static io.thedevjournal.java.common.NumberConstants.TWO;
 import static io.thedevjournal.java.common.StringConstants.EMPTY_STRING;
@@ -19,10 +20,14 @@ public class DeFront {
 
         final int inputLength = inputNormalized.length();
 
-        final String first = inputLength > 0 && CHAR_SMALL_A == inputNormalized.charAt(0) ? CHAR_SMALL_A.toString() : EMPTY_STRING;
-        final String second = inputLength > ONE && CHAR_SMALL_B == inputNormalized.charAt(ONE) ? CHAR_SMALL_B.toString() : EMPTY_STRING;
+        final String first = getMatchingCharacterAtIndex(inputNormalized, ZERO, CHAR_SMALL_A);
+        final String second = getMatchingCharacterAtIndex(inputNormalized, ONE, CHAR_SMALL_B);
         final String rest = inputLength > TWO ? inputNormalized.substring(TWO) : EMPTY_STRING;
 
         return first + second + rest;
+    }
+
+    public String getMatchingCharacterAtIndex(final String input, final int index, final Character character) {
+        return input.length() > index && character == input.charAt(index) ? character.toString() : EMPTY_STRING;
     }
 }
