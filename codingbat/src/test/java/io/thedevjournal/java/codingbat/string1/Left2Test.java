9512aeff3a7acfb203721973e1da1f2bae9edf22
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-02-04 20:50:00 +0530

    GH-80: Adds Tests for CodingBat - String-1 - left2

---
+++ codingbat/src/test/java/io/thedevjournal/java/codingbat/string1/Left2Test.java
@@ -0,0 +1,101 @@
+package io.thedevjournal.java.codingbat.string1;
+
+import static io.thedevjournal.java.common.StringConstants.EMPTY_STRING;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Left2Test {
+
+    private Left2 fixture;
+
+    @BeforeEach
+    void setUp() {
+        fixture = new Left2();
+    }
+
+    @Test
+    void testcase1() {
+
+        final String input = "Hi";
+
+        final String expected = "Hi";
+
+        final String actual = fixture.left2(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testcase2() {
+
+        final String input = "java";
+
+        final String expected = "vaja";
+
+        final String actual = fixture.left2(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testcase3() {
+
+        final String input = "Hello";
+
+        final String expected = "lloHe";
+
+        final String actual = fixture.left2(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnEmpty_WhenInputIsNull() {
+
+        final String input = null;
+
+        final String expected = EMPTY_STRING;
+
+        final String actual = fixture.left2(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnEmpty_WhenInputIsEmpty() {
+
+        final String input = EMPTY_STRING;
+
+        final String expected = EMPTY_STRING;
+
+        final String actual = fixture.left2(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnSameString_WhenInputIsSingleChar() {
+
+        final String input = "a";
+
+        final String expected = "a";
+
+        final String actual = fixture.left2(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldReturnSameString_WhenInputIsDoubleChar() {
+
+        final String input = "ab";
+
+        final String expected = "ab";
+
+        final String actual = fixture.left2(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
