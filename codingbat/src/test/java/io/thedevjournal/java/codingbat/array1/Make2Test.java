f275e8825c8b82c747bfb97a313425c642852e8b
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-02-16 21:18:00 +0530

    GH-107: Fixes Codacy violation - make2

--- codingbat/src/test/java/io/thedevjournal/java/codingbat/array1/Make2Test.java
+++ codingbat/src/test/java/io/thedevjournal/java/codingbat/array1/Make2Test.java
@@ -16,36 +16,36 @@ class Make2Test {
 
     @Test
     void testcase1() {
-        final int[] a = new int[] { 4, 5 };
-        final int[] b = new int[] { 1, 2, 3 };
+        final int[] arrayA = { 4, 5 };
+        final int[] arrayB = { 1, 2, 3 };
 
-        final int[] expected = new int[] { 4, 5 };
+        final int[] expected = { 4, 5 };
 
-        final int[] actual = fixture.make2(a, b);
+        final int[] actual = fixture.make2(arrayA, arrayB);
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void testcase2() {
-        final int[] a = new int[] { 4 };
-        final int[] b = new int[] { 1, 2, 3 };
+        final int[] arrayA = { 4 };
+        final int[] arrayB = { 1, 2, 3 };
 
-        final int[] expected = new int[] { 4, 1 };
+        final int[] expected = { 4, 1 };
 
-        final int[] actual = fixture.make2(a, b);
+        final int[] actual = fixture.make2(arrayA, arrayB);
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void testcase3() {
-        final int[] a = new int[] {};
-        final int[] b = new int[] { 1, 2 };
+        final int[] arrayA = {};
+        final int[] arrayB = { 1, 2 };
 
-        final int[] expected = new int[] { 1, 2 };
+        final int[] expected = { 1, 2 };
 
-        final int[] actual = fixture.make2(a, b);
+        final int[] actual = fixture.make2(arrayA, arrayB);
 
         assertThat(actual).isEqualTo(expected);
     }
