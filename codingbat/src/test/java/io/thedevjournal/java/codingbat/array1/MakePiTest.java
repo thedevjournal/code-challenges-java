8ec1f1cb3d157495963665b43c1c81c35c1b209b
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-02-18 20:33:00 +0530

    GH-111: Adds Tests for CodingBat - Array-1 - makePi

---
+++ codingbat/src/test/java/io/thedevjournal/java/codingbat/array1/MakePiTest.java
@@ -0,0 +1,25 @@
+package io.thedevjournal.java.codingbat.array1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MakePiTest {
+
+    private MakePi fixture;
+
+    @BeforeEach
+    void setUp() {
+        fixture = new MakePi();
+    }
+
+    @Test
+    void testcase1() {
+        int[] expected = new int[] { 3, 1, 4 };
+
+        int[] actual = fixture.makePi();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
