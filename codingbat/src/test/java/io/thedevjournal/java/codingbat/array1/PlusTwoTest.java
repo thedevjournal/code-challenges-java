3384c35c5db912ed163e18ea91ed77513399cc1c
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-02-20 22:27:56 +0530

    GH-117: Fixes Codacy violation - plusTwo

--- codingbat/src/test/java/io/thedevjournal/java/codingbat/array1/PlusTwoTest.java
+++ codingbat/src/test/java/io/thedevjournal/java/codingbat/array1/PlusTwoTest.java
@@ -16,36 +16,36 @@ class PlusTwoTest {
 
     @Test
     void testcase1() {
-        final int[] a = { 1, 2 };
-        final int[] b = { 3, 4 };
+        final int[] arrayA = { 1, 2 };
+        final int[] arrayB = { 3, 4 };
 
         final int[] expected = { 1, 2, 3, 4 };
 
-        final int[] actual = fixture.plusTwo(a, b);
+        final int[] actual = fixture.plusTwo(arrayA, arrayB);
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void testcase2() {
-        final int[] a = { 4, 4 };
-        final int[] b = { 2, 2 };
+        final int[] arrayA = { 4, 4 };
+        final int[] arrayB = { 2, 2 };
 
         final int[] expected = { 4, 4, 2, 2 };
 
-        final int[] actual = fixture.plusTwo(a, b);
+        final int[] actual = fixture.plusTwo(arrayA, arrayB);
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void testcase3() {
-        final int[] a = { 9, 2 };
-        final int[] b = { 3, 4 };
+        final int[] arrayA = { 9, 2 };
+        final int[] arrayB = { 3, 4 };
 
         final int[] expected = { 9, 2, 3, 4 };
 
-        final int[] actual = fixture.plusTwo(a, b);
+        final int[] actual = fixture.plusTwo(arrayA, arrayB);
 
         assertThat(actual).isEqualTo(expected);
     }
