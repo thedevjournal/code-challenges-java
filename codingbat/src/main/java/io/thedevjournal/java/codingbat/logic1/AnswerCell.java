f188aab46c2ecaedae7bee73b7acbc8b9e582759
The Dev Journal <168292748+thedevjournal@users.noreply.github.com>
2024-02-26 21:08:17 +0530

    GH-127: Resolves Codacy violations

--- codingbat/src/main/java/io/thedevjournal/java/codingbat/logic1/AnswerCell.java
+++ codingbat/src/main/java/io/thedevjournal/java/codingbat/logic1/AnswerCell.java
@@ -2,7 +2,7 @@ package io.thedevjournal.java.codingbat.logic1;
 
 public class AnswerCell {
 
-    public boolean answerCell(final boolean isMorning, boolean isMom, boolean isAsleep) {
+    public boolean answerCell(final boolean isMorning, final boolean isMom, final boolean isAsleep) {
 
         boolean result = true;
 
